1690f05852fa6c4a8eddb8379cbc814d31265793
simtaxx <souici.haris@gmail.com>
2019-06-08 12:41:36 +0200

    vue js added

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Ceci est une 2 eme page</h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Component msg="Ceci est un composante de base avec Vue"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Component from "../components/Component.vue";
+
+export default {
+  name: "home",
+  components: {
+    Component
+  }
+};
+</script>
